b035e0c27b0c2c04735ab3791d40b37c29b52410
James Seung Hyun Son <jamessoun93@gmail.com>
2022-06-12 21:55:34 +0900

    spring aop

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -18,6 +18,7 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+        System.out.println("memberService = " + memberService.getClass()); // 프록시 주입 확인
     }
 
     @GetMapping("/members/new")
